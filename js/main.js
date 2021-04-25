526da75227c5e78a4b548ccd16a78bd8bc955255
habib <habibdev96@gmail.com>
2021-04-25 16:31:05 -0600

    added scroll spy functionality

--- js/main.js
+++ js/main.js
@@ -10,6 +10,57 @@ const preloader = () => {
 };
 preloader();
 
+// scroll spy
+const scrollSpy = () => {
+  const options = { rootMargin: "-400px" };
+
+  // watch products
+  const productsSection = document.querySelector("#products");
+  const productsNavLink = document.querySelector(".products");
+
+  const observeProducts = new IntersectionObserver(function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        productsNavLink.classList.add("active");
+      } else {
+        productsNavLink.classList.remove("active");
+      }
+    });
+  }, options);
+  observeProducts.observe(productsSection);
+
+  // watch partners
+  const partnersSection = document.querySelector("#partners");
+  const partnersNavLink = document.querySelector(".partners");
+
+  const observePartners = new IntersectionObserver(function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        partnersNavLink.classList.add("active");
+      } else {
+        partnersNavLink.classList.remove("active");
+      }
+    });
+  }, options);
+  observePartners.observe(partnersSection);
+
+  // watch careers
+  const careersSection = document.querySelector("#careers");
+  const careersNavLink = document.querySelector(".careers");
+
+  const observeCareers = new IntersectionObserver(function (entries) {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        careersNavLink.classList.add("active");
+      } else {
+        careersNavLink.classList.remove("active");
+      }
+    });
+  }, options);
+  observeCareers.observe(careersSection);
+};
+scrollSpy();
+
 // partners carousel
 const partnersCarousel = () => {
   const width = window.innerWidth;
