ba8196e817e2936b2886fed2417a516095e8c15a
habib <habibdev96@gmail.com>
2021-04-25 15:07:23 -0600

    created preloader

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 "use strict";
 
+// preloader
+const preloader = () => {
+  const preloaderEl = document.querySelector(".preloader");
+
+  window.addEventListener("load", function () {
+    preloaderEl.classList.add("hide-preloader");
+  });
+};
+preloader();
+
 // partners carousel
 const partnersCarousel = () => {
   const width = window.innerWidth;
