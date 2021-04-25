7d20baeecf11a20fe5a2a10df0ab1d9ca51e9e20
habib <habibdev96@gmail.com>
2021-04-25 12:18:03 -0600

    created partners carousel

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
 "use strict";
+
+// partners carousel
+const partnersCarousel = () => {
+  const width = window.innerWidth;
+
+  const partnersObj = {
+    type: "carousel",
+    autoplay: 1500,
+    perView: 4,
+  };
+
+  if (width <= 650) {
+    partnersObj.perView = 3;
+  }
+
+  new Glide(".glide", partnersObj).mount();
+};
+partnersCarousel();
